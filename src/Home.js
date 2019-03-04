66285a526a4b102d13ad3936d695f66f297c84c2
github username <github email address>
2019-03-04 18:06:24 -0500

    login page

--- src/Home.js
+++ src/Home.js
@@ -5,14 +5,23 @@ import React, { Component } from 'react';
 import MainContainer from './containers/MainContainer'
 import Header from './components/Header';
 import './App.css';
+import './layouts/Home.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-      You are not logged in! Please login to use app.
-
+      <div className="login">
+      <div className="loginForm">
+      <h1>Let's Spoon <i className="fa fa-spoon"></i></h1>
+      <h2> Please log in, to start exploring restaurants</h2>
+      <form>
+      <label><b>Email: </b></label>
+      <input type="text" name="email"></input><br/>
+      <label><b>Password:</b></label>
+      <input type="text" name="password"></input><br/>
+      <button type="submit">Enter</button>
+      </form>
+      </div>
       </div>
     );
   }
